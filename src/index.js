273df038e9395eef2a345b246493be267da71069
edu.ministori@gmail.com <edu.ministori@gmail.com>
2021-09-03 12:05:53 +0900

    add Dev

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AppEvent from './AppEvent';
+// import AppEvent from './AppEvent';
 import AppArray from './AppArray';
 
 ReactDOM.render(
